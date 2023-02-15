576e3270218f419b1247a3eb4b69e5d2907ed3fe
Miki-L21 <122608792+Miki-L21@users.noreply.github.com>
2023-02-15 09:56:38 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+let distancia = false
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 40 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+        distancia = Math.randomBoolean()
+        if (distancia == true) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 144)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.pause(500)
+        }
+        if (distancia == false) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 144)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.pause(500)
+        }
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+})
